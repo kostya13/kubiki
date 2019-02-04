e43bc10182fef5501538e8bb4adb6aee0571f915
Konstantin Ilyashenko <konstantin_ilyashenko@epam.com>
2019-02-04 16:14:07 +0400

    Доработана документация. Обновлен пример.

--- examples/lesson01.py
+++ examples/lesson01.py
@@ -1,29 +1,35 @@
 "Разбиение кода на функции. Рисование осей"
 from kubiki.mc import mc, block
 
-золото = block.GOLD_BLOCK
-песок = block.SAND
-кирпич = block.BRICK_BLOCK
-длина = 20
 
-def ось_X():
-    for i in range(1, длина):
-        mc.setBlock(i, 0, 0, золото)
+block_X = block.GOLD_BLOCK
+block_Y = block.SAND
+block_Z = block.BRICK_BLOCK
+length = 20
 
-def ось_Y():
-    for i in range(1, длина):
-        mc.setBlock(0, i, 0, песок)
 
-def ось_Z():
-    for i in range(1, длина):
-        mc.setBlock(0, 0, i, кирпич)
+def axis_X():
+    for i in range(1, length):
+        mc.setBlock(i, 0, 0, block_X)
 
-def нарисовать_оси():
-    ось_X()
-    ось_Y()
-    ось_Z()
 
-нарисовать_оси()
+def axis_Y():
+    for i in range(1, length):
+        mc.setBlock(0, i, 0, block_Y)
+
+
+def axis_Z():
+    for i in range(1, length):
+        mc.setBlock(0, 0, i, block_Z)
+
+
+def draw_axes():
+    axis_X()
+    axis_Y()
+    axis_Z()
+
+
+draw_axes()
 
 
 
